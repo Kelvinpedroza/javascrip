5d58f286537ead059f47ec2188f0da16188f2713
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2022-08-03 20:55:36 -0300

    uper

--- main.js
+++
@@ -1,8 +0,0 @@
-const para = document.querySelector('p')
-
-para.addEventListener('click',atualizarNome);
-
-function atualizarNome(){
-    var nome = prompt('Insira um novo nome ');
-    para.textContent='JOgador 1 ' + nome;
-}
