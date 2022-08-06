ccbd53a2f418ac73ae57b44f8e3b1acfaecd8a8e
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2022-08-06 19:08:38 -0300

    aulas

---
+++ aulasJavascrip/tipoString.js
@@ -0,0 +1,51 @@
+/*
+    String 
+        *cadeia de caracteres
+        "" // aspas duplas
+        '' // aspas simples
+        ´´ // template liretais ou templates string da para fazer mult-linhas
+
+
+    console.log(`mayk ${1 + 1}`)
+    */
+
+    /* 
+        Number
+        * numeros
+        33 // inteiros
+        12.5 // reais
+        NaN // not a Number
+        infinity // infinito
+
+
+    console.log(33 === Infinity)
+
+    */
+   
+/* 
+    *object 
+    * Propriedades / Atributos
+    * funcionalidades / metodos
+    {Propriedade: "valor"}
+    
+    console.log({
+        name: "mayk",
+        idade: 36,
+        andar: function(){
+        console.log('andar')
+    }
+
+})
+*/
+
+
+
+/* Array (Vetores)
+    * Uma lista
+    * Agrupamento de dados
+    ["maryk",26]
+console.log([
+    "Leite",
+    "voas",
+   "carro",
+ */   
---
+++ aulasJavascrip/variaveis.js
@@ -0,0 +1,5 @@
+let maconha = "pronta"
+let fogo ="aceso"
+if(maconha ==="pronta" && fogo ==="aceso"){
+ console.log("bora fumar")
+}
