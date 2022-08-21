df59e8ad4a5869927ddf7bf4571ec29affd2f3de
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2022-08-21 20:31:11 -0300

    Update encadeamento.js

--- funcional/encadeamento.js
+++ funcional/encadeamento.js
@@ -1,1 +1,6 @@
+// composição de funções 
+// · Um encadeamento de funções 
+// · Uma função que retorna uma do e vai para outra função que retorna um dado e vai para outra função  que retorna ..
 
+const people = ['rafa','Diego','Dani','Rod']
+const upperCasePeopleThatStartsWithd = people.filter(person => person.startsWith('D')).map(dperson => dperson.toLocaleUpperCase())
