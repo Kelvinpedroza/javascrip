a16126280843a3d324d74f4b9525c2f32e467074
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2022-08-21 16:17:30 -0300

    UP

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -4,11 +4,42 @@ const app = express()
 app.set("view engine","ejs")
 
 app.get("/", function(req,res){
-    res.render('index')
-})
+    const items = [
+        {
+            title: 'D',
+            message: 'esenvolver aplicações/serviços de forma fácil'
+        },
+        {
+            title: 'E',
+            message: 'JS usa JavaScript para rendenrizar HTML'
 
+        },
+        {
+            title: 'M',
+            message:'utio facil de usar'
+        },
+        {
+            title: 'A',
+            message: 'morzinho'
+
+        },
+        {
+            title: 'I',
+            message: 'nsatall ejs'
+        },
+        {
+            title: 'S',
+            message: 'intaxe simples'
+        }
+
+    ]
+    res.render('pages/index' ,{
+        qualitys:items,
+    })
+})
+const  subtitle ='Uma linguagem dee modelagem para ciração em html'
 app.get("/sobre", function(req,res){
-    res.render('about')
+    res.render('pages/about')
 })
 
 
