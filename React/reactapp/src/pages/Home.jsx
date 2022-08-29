7448ab26cf0987479f9f1e4d07f7bc28f583092c
Kelvin Pedroza <109870376+Kelvinpedroza@users.noreply.github.com>
2022-08-29 11:20:22 -0300

    Upadate

---
+++ React/reactapp/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+       <h1>Lista de Presença</h1>
+    <input type="text" name="iname" id="iname" placeholder="Digite seu nome" />
+    <button type="button">Adicionar</button>
+    
+    </>
+  )
+  
+}
+
+export default Home
